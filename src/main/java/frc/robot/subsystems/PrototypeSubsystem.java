eb6af0c0fa07aa43a14e38fc9aac97190bbbf197
Victor Chen <victorc.1@outlook.com>
2022-01-15 11:36:46 -0500

    Create PrototypeSubsystem
    A subsystem that provides methods to control the four motors that are currently not connected to anything.

---
+++ src/main/java/frc/robot/subsystems/PrototypeSubsystem.java
@@ -0,0 +1,73 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * Do not use this in production! This subsystem creates bindings to control
+ * arbitrary speed controllers. Currently, this is being used so that the four
+ * speed controllers that are not connected to anything can be controlled via
+ * joysticks so that the mechanical teams are able to use it to prototype their
+ * designs.
+ * 
+ * @author Victor Chen <victorc.1@outlook.com>
+ */
+public class PrototypeSubsystem extends SubsystemBase {
+
+    private final CANSparkMax m_motor1 = new CANSparkMax(0, MotorType.kBrushless); // TODO: Change the device number
+    private final TalonFX m_motor2 = new TalonFX(1); // TODO: Change the device number
+    private final TalonFX m_motor3 = new TalonFX(2); // TODO: Change the device number
+    private final TalonFX m_motor4 = new TalonFX(3); // TODO: Change the device number
+
+    /**
+     * Constructor.
+     */
+    public PrototypeSubsystem() {
+
+    }
+
+    /**
+     * Runs motor 1, the SPARK MAX, at the set {@code speed}, which is a value
+     * between [-1, 1].
+     * 
+     * @param speed the speed to run the motor at
+     */
+    public void runMotor1(double speed) {
+        m_motor1.set(speed);
+    }
+
+    /**
+     * Runs motor 2, a TalonFX, at the set {@code speed}, which is a value between
+     * [-1, 1].
+     * 
+     * @param speed the speed to run the motor at
+     */
+    public void runMotor2(double speed) {
+        m_motor2.set(TalonFXControlMode.PercentOutput, speed);
+    }
+
+    /**
+     * Runs motor 3, a TalonFX, at the set {@code speed}, which is a value between
+     * [-1, 1].
+     * 
+     * @param speed the speed to run the motor at
+     */
+    public void runMotor3(double speed) {
+        m_motor3.set(TalonFXControlMode.PercentOutput, speed);
+    }
+
+    /**
+     * Runs motor 4, a TalonFX, at the set {@code speed}, which is a value between
+     * [-1, 1].
+     * 
+     * @param speed the speed to run the motor at
+     */
+    public void runMotor4(double speed) {
+        m_motor4.set(TalonFXControlMode.PercentOutput, speed);
+    }
+
+}
