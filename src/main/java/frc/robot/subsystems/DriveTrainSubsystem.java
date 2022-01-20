16fa82620531d87a5523eb3d18ecc3fe997fb2bf
DivNandi <94492291+DivNandi@users.noreply.github.com>
2022-01-19 19:21:24 -0500

    deleted comments and unused import, Drivetrainsubsystem

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -1,25 +1,12 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.DriveTrainConstants;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 
 public class DriveTrainSubsystem extends SubsystemBase {
-
-    /*
-     * TODO: With our current hardware of SPARK MAX speed controllers, this code
-     * does not work! Modify the code to use SPARK MAX speed controllers instead of
-     * PWMTalonFX speed controllers. To do this, you may need to install a vendor
-     * library for REV Robotics.
-     */
-
-    // private final PWMTalonFX m_rightMotor = new PWMTalonFX(DriveTrainConstants.kDriveTrainRightPort);
-    // private final PWMTalonFX m_leftMotor = new PWMTalonFX(DriveTrainConstants.kDriveTrainLeftPort);
-    
-
     private final CANSparkMax m_frontRight = new CANSparkMax(1, CANSparkMaxLowLevel.MotorType.kBrushless);
     private final CANSparkMax m_frontLeft = new CANSparkMax(2, CANSparkMaxLowLevel.MotorType.kBrushless);
     private final CANSparkMax m_backRight = new CANSparkMax(3, CANSparkMaxLowLevel.MotorType.kBrushless);
