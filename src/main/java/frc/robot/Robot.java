bbfab0bffefb6011c92ca1129920d6db7d581e1b
wxblank <wxblank@gmail.com>
2022-01-28 19:27:55 -0500

    Add BadLog

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -8,6 +8,8 @@ import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import badlog.lib.BadLog;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -18,6 +20,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  private BadLog m_logger;
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -28,6 +31,14 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    System.out.println("Initializing logging...");
+    initLogging();
+  }
+
+  private void initLogging() {
+    m_logger = BadLog.init("test.bag");
+    m_logger.finishInitialization();
   }
 
   /**
@@ -39,6 +50,8 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotPeriodic() {
+    m_logger.log();
+
     // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
     // commands, running already-scheduled commands, removing finished or interrupted commands,
     // and running subsystem periodic() methods.  This must be called from the robot's periodic
