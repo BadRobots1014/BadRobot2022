4caafc9e51d35629f9f8cf102fbf257d44fd5e7d
Benjamin <24gluck_benjamin@dublinstudents.net>
2022-02-08 15:44:38 -0500

    Added some files

--- src/main/java/frc/robot/commands/AutoShootCommand.java
+++ src/main/java/frc/robot/commands/AutoShootCommand.java
@@ -1,21 +1,25 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import frc.robot.subsystems.DriveTrainSubsystem;
 import frc.robot.subsystems.GyroSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 
-public class AutoShootCommand extends SequentialCommandGroup {
+public class AutoShootCommand extends ParallelDeadlineGroup {
     public AutoShootCommand (
         DriveTrainSubsystem driveSubsystem, 
         GyroSubsystem gyro, 
         ShooterSubsystem shooterSubsystem
         ) {
-        addCommands(
-            // todo: make a command that orients towards hub instead of pivot turn
-            DriveStrategyCommand.pivotTurn(driveSubsystem, () -> 56.0),
-            DriveStrategyCommand.anchor(driveSubsystem, gyro),
-            new ShootCommand(shooterSubsystem)
+        super(
+            // Robot is turned on facing the hub so this should work
+            new ShootCommand(shooterSubsystem),
+            new ParallelCommandGroup
+            (
+            DriveStrategyCommand.pivotTurn(driveSubsystem, () -> 0.0),
+            DriveStrategyCommand.anchor(driveSubsystem, gyro)
+            )
         );
     }
 }
