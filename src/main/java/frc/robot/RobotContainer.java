161e92eeecb9239596c67f39a0dcd5c67276bd55
Victor Chen <victorc.1@outlook.com>
2022-03-10 18:08:22 -0500

    Fix merge hiccups
    Remove references to PrototypeSubsystem, etc.
    Remove duplicate code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,9 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -15,7 +18,6 @@ import frc.robot.Constants.ControllerConstants;
 import frc.robot.commands.DeployGathererCommand;
 import frc.robot.commands.BeginGatheringCommand;
 import frc.robot.commands.AutoStartRoutineCommand;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.ShootCommand;
 import frc.robot.commands.TeleopDriveCommand;
 import frc.robot.subsystems.GathererSubsystem;
@@ -60,64 +62,11 @@ public class RobotContainer {
     m_gyroSubsystem,
     m_shooterSubsystem
   );
-  private final ShootCommand m_shootCommand = new ShootCommand(m_shooterSubsystem);
-  private final TeleopDriveCommand m_teleopDriveCommand = new TeleopDriveCommand(
-    m_driveTrainSubsystem,
-    m_gyroSubsystem,
-    m_visionSubsystem,
-    () -> {
-      // Invert the X-axis.
-      return -1.0 * m_driverStick.getX();
-    },
-    m_driverStick::getY,
-    () -> {
-      if (m_driverStick.getRawButton(ControllerConstants.kThrottleButton)) {
-        return 0.50;
-      } else {
-        // The default throttle is 75%. In practice, however, the maximum motor power is 56%, as the
-        // {@link DriveTrainSubsystem#tankDrive} currently squares inputs.
-        return 0.75;
-      }
-    },
-    () -> {
-      // TODO
-      return m_driverStick.getRawButton(ControllerConstants.kFollowTargetButton);
-    }
-  );
-
-  /**
-   * The joystick used for prototyping controls.
-   */
-  private final Joystick m_prototypeJoystick = new Joystick(ControllerConstants.kPrototypePort);
-
-  /**
-   * The {@code PrototypeSubsystem} used for prototyping.
-   */
-  private final PrototypeSubsystem m_prototypeSubsystem = new PrototypeSubsystem();
-
-  /**
-   * The {@code PrototypeControlCommand} used to control the
-   * {@code PrototypeSubsystem}.
-   */
-  private final PrototypeControlCommand m_prototypeControlCommand = new PrototypeControlCommand(m_prototypeSubsystem,
-      this::getPrototypePowerOutput);
-
-  /**
-   * The chooser for the input to drive the prototyping speed controllers.
-   * Configured to accept input from the joystick or from a discrete value entered
-   * into Shuffleboard.
-   */
-  private final SendableChooser<Boolean> m_prototypeInputChooser = new SendableChooser<>();
 
   /**
    * Chooser for the automonous routine
    */
   private final SendableChooser<Command> m_automonousRoutineChooser = new SendableChooser<>();
-
-  /**
-   * The speed [0, 1] to drive the prototyping speed controllers at.
-   */
-  private final NetworkTableEntry m_prototypePower;
 
   private final ShootCommand m_shootCommand = new ShootCommand(m_shooterSubsystem);
   private final TeleopDriveCommand m_teleopDriveCommand = new TeleopDriveCommand(
@@ -145,11 +94,6 @@ public class RobotContainer {
       });
 
   /**
-   * Chooser for the automonous routine
-   */
-  private final SendableChooser<Command> m_automonousRoutineChooser = new SendableChooser<>();
-
-  /**
    * A reference to the "Autonomous" Shuffleboard tab object.
    */
   private final ShuffleboardTab m_autonomousTab = Shuffleboard.getTab("Autonomous");
@@ -166,20 +110,6 @@ public class RobotContainer {
     // Configure the autonomous routine chooser
     m_automonousRoutineChooser.setDefaultOption("Start Routine", m_autoStartCommand);
     m_autonomousTab.add("Routine", m_automonousRoutineChooser).withWidget(BuiltInWidgets.kComboBoxChooser);
-
-    // Configure the prototype input chooser
-    m_prototypeInputChooser.setDefaultOption("Joystick", true);
-    m_prototypeInputChooser.addOption("Discrete Value", false);
-
-    m_prototypeTab.add("Input Source", m_prototypeInputChooser).withWidget(BuiltInWidgets.kComboBoxChooser);
-
-    // Configure the discrete power output chooser
-    m_prototypePower = m_prototypeTab.add("Power Output (Discrete)", 0).withWidget(BuiltInWidgets.kNumberSlider)
-        .withProperties(Map.of("min", 0, "max", 1)).getEntry();
-
-    // Configure the invert output chooser
-    m_prototypeOutputIsInverted = m_prototypeTab.add("Invert Output", false).withWidget(BuiltInWidgets.kToggleSwitch)
-        .getEntry();
     
     // Configure the button bindings
     configureButtonBindings();
@@ -234,20 +164,5 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     return m_automonousRoutineChooser.getSelected();
-  }
-
-  /**
-   * Calculates the speed value to drive the prototype speed controller at.
-   *
-   * @return the speed [-1, 1] to drive the prototype speed controller at.
-   */
-  private double getPrototypePowerOutput() {
-    double inversionConstant = m_prototypeOutputIsInverted.getBoolean(false) ? 1 : -1;
-
-    if (m_prototypeInputChooser.getSelected()) {
-      return inversionConstant * m_prototypeJoystick.getY();
-    } else {
-      return inversionConstant * m_prototypePower.getDouble(0);
-    }
   }
 }
