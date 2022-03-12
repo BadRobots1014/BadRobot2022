f1c97199215efc80ddc119d63e3ceaf19d6250de
Victor Chen <victorc.1@outlook.com>
2022-03-12 14:10:09 -0500

    use WPILib Timer in ShootCommand, add docs

--- src/main/java/frc/robot/commands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -1,63 +1,99 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ShooterSubsystem;
-import frc.robot.Constants.ShooterConstants;
 import frc.robot.subsystems.IndexerSubsystem;
 
-
 public class ShootCommand extends CommandBase {
-    
-    private final ShooterSubsystem m_subsystem;
-    private final IndexerSubsystem m_indexSubsystem;
-    private final double m_power;
-    private boolean runIndexer;
-    
-    public ShootCommand(ShooterSubsystem subsystem, IndexerSubsystem indexSubsystem, double power) {
-        m_subsystem = subsystem;
-        m_indexSubsystem = indexSubsystem;
-        m_power = power;
 
-        addRequirements(subsystem);
+    /*
+     * Private constants ------------------------------------------------------
+     */
+
+    /**
+     * The amount of time, in seconds, to wait between activating the shooter motor
+     * and activating the indexer motor.
+     */
+    private static final double WAIT_TIME = 1.0;
+
+    /*
+     * Private instance members -----------------------------------------------
+     */
+
+    /**
+     * The {@link ShooterSubsystem} controlled by this command.
+     */
+    private final ShooterSubsystem shooter;
+
+    /**
+     * The {@link IndexerSubsystem} controlled by this command.
+     */
+    private final IndexerSubsystem indexer;
+
+    /**
+     * A {@link Timer} used to implement a delay between activating the shooter
+     * motor and activating the indexer motor.
+     */
+    private final Timer timer;
+
+    /**
+     * The percentage output to drive the shooter at.
+     */
+    private final double power;
+
+    /*
+     * Constructor ------------------------------------------------------------
+     */
+
+    /**
+     * Constructs a new {@link ShootCommand}, parameterized by the subsystems
+     * ({@code shooter}, {@code indexer}) to control, and the shooter power output.
+     * 
+     * @param shooter the {@link ShooterSubsystem} controlled by this command
+     * @param indexer the {@link IndexerSubsystem} controlled by this command
+     * @param power   the percentage output to run the shooter at
+     * @requires {@code power} is in the range [-1.0, 1.0]
+     */
+    public ShootCommand(ShooterSubsystem shooter, IndexerSubsystem indexer, double power) {
+        this.shooter = shooter;
+        this.indexer = indexer;
+
+        this.timer = new Timer();
+
+        this.power = power;
+
+        addRequirements(shooter, indexer);
     }
 
-    // Called when the command is initially scheduled.
+    /*
+     * Command methods --------------------------------------------------------
+     */
+
     @Override
     public void initialize() {
-        //m_subsystem.run(0.5);
-        runIndexer = false;
-        try {
-            m_indexSubsystem.wait(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        runIndexer = true;
+        this.timer.reset();
+        this.timer.start();
+
+        this.shooter.run(this.power);
     }
 
-    // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        m_subsystem.run(m_power);
-        // if (Math.abs(m_subsystem.getDeltaDesiredVelocity()) < ShooterConstants.kMaxTolerance) {
-        //     m_indexSubsystem.runUpperMotor();
-        // }
-        // else {
-        //     m_indexSubsystem.stopUpperMotor();
-        // }
-        if (runIndexer) {
-            m_indexSubsystem.runUpperMotor();
+        this.shooter.run(this.power);
+
+        if (this.timer.hasElapsed(WAIT_TIME)) {
+            this.indexer.runUpperMotor();
         }
     }
 
-    // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        m_subsystem.stop();
-        m_indexSubsystem.stopUpperMotor();
+        this.shooter.stop();
+        this.indexer.stopUpperMotor();
         System.out.println("Stopping everything");
     }
 
-    // Returns true when the command should end.
     @Override
     public boolean isFinished() {
         return false;
