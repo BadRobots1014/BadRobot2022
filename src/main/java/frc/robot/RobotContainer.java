5e114a1565049e5890c4ecec88197f154daca45c
Victor Chen <victorc.1@outlook.com>
2022-03-12 17:41:04 -0500

    organize RobotContainer, eliminate lambda funcs

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,12 +4,9 @@
 
 package frc.robot;
 
-import java.util.function.BooleanSupplier;
-
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.XboxController.Button;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -32,129 +29,198 @@ import frc.robot.commands.drive.TeleopDriveCommand;
 
 /**
  * This class is where the bulk of the robot should be declared. Since
- * Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in
- * the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of
- * the robot (including
- * subsystems, commands, and button mappings) should be declared here.
+ * Command-based is a "declarative" paradigm, very little robot logic should
+ * actually be handled in the {@link Robot} periodic methods (other than the
+ * scheduler calls). Instead, the structure of the robot (including subsystems,
+ * commands, and button mappings) should be declared here.
  */
 public class RobotContainer {
-    // The robot's subsystems and commands are defined here...
-    // TODO: Fix this constant
-    private final Joystick m_driverStick = new Joystick(0);
-    private final XboxController m_shooterController = new XboxController(1);
 
-    private final VisionSubsystem m_visionSubsystem = new VisionSubsystem();
-    private final ShooterSubsystem m_shooterSubsystem = new ShooterSubsystem();
-    private final DriveTrainSubsystem m_driveTrainSubsystem = new DriveTrainSubsystem();
-    private final GyroSubsystem m_gyroSubsystem = new GyroSubsystem();
-    private final GathererSubsystem m_gathererSubsystem = new GathererSubsystem();
-    private final IndexerSubsystem m_indexerSubsystem = new IndexerSubsystem();
+    /*
+     * Private constants ------------------------------------------------------
+     */
 
-    private final DeployGathererCommand m_deployGathererCommand = new DeployGathererCommand(m_gathererSubsystem);
-    private final RetractGathererCommand m_retractGathererCommand = new RetractGathererCommand(m_gathererSubsystem);
-    private final BeginGatheringCommand m_startGathererCommand = new BeginGatheringCommand(m_gathererSubsystem);
+    /*
+     * The port number for the primary controller.
+     */
+    private static final int PRIMARY_CONTROLLER_PORT = 0;
 
-    private final IndexerCommand m_runIndexerCommand = new IndexerCommand(m_indexerSubsystem);
-    private final UpperIndexerCommand m_runUpperIndexerCommand = new UpperIndexerCommand(m_indexerSubsystem);
-
-    private final ShootCommand m_shootCommand = new ShootCommand(m_shooterSubsystem, m_indexerSubsystem, 0.5);
-    private final ShootCommand m_shootBackCommand = new ShootCommand(m_shooterSubsystem, m_indexerSubsystem, -0.5);
-    private final ShootCommand m_closeShootCommand = new ShootCommand(m_shooterSubsystem, m_indexerSubsystem, 0.25);
-    private final ShootCommand m_closeShootBackCommand = new ShootCommand(m_shooterSubsystem, m_indexerSubsystem, -0.25);
-    private final TeleopDriveCommand m_teleopDriveCommand = new TeleopDriveCommand(
-            m_driveTrainSubsystem,
-            m_gyroSubsystem,
-            () -> {
-                // Invert the X-axis.
-                return -1.0 * m_driverStick.getX();
-            },
-            m_driverStick::getY,
-            () -> {
-                if (m_driverStick.getRawButton(ControllerConstants.kThrottleButton)) {
-                    return 0.50;
-                } else {
-                    // The default throttle is 75%. In practice, however, the maximum motor power is
-                    // 56%, as the
-                    // {@link DriveTrainSubsystem#tankDrive} currently squares inputs.
-                    return 1.0;
-                }
-            });
-
-    private final FollowTargetCommand m_followTargetCommand = new FollowTargetCommand(m_driveTrainSubsystem,
-            m_visionSubsystem, () -> {
-                // Invert the X-axis.
-                return -1.0 * m_driverStick.getX();
-            }, m_driverStick::getY);
+    /*
+     * The port number for the secondary controller.
+     */
+    private static final int SECONDARY_CONTROLLER_PORT = 1;
 
     /**
-     * Chooser for the automonous routine
+     * The throttle value to use with {@link #teleopDriveCmd} normally.
      */
-    private final SendableChooser<Command> m_automonousRoutineChooser = new SendableChooser<>();
+    private static final double NORMAL_THROTTLE = 1.0;
 
     /**
-     * The container for the robot. Contains subsystems, OI devices, and commands.
+     * The throttle value to use with {@link #teleopDriveCmd} in slow mode.
      */
-    public RobotContainer() {
-        System.out.println("RobotContainer initializing");
-        // We use an arcade-drive system with joystick regions that select specialized
-        // drive strategies.
-        m_driveTrainSubsystem.setDefaultCommand(m_teleopDriveCommand);
+    private static final double SLOW_THROTTLE = 0.5;
 
-        // Configure the button bindings
-        configureButtonBindings();
-        System.out.println("RobotContainer Init done");
+    /**
+     * The threshold reading value used to deterimne if a trigger is depressed.
+     */
+    private static final double TRIGGER_THRESHOLD = 0.3;
+
+    /*
+     * Controllers ------------------------------------------------------------
+     */
+
+    private final Joystick primaryController;
+    private final XboxController secondaryController;
+
+    /*
+     * Subsystems -------------------------------------------------------------
+     */
+
+    private final GyroSubsystem gyroSubsystem;
+    private final VisionSubsystem visionSubsystem;
+
+    private final DriveTrainSubsystem driveTrainSubsystem;
+
+    private final GathererSubsystem gathererSubsystem;
+    private final IndexerSubsystem indexerSubsystem;
+    private final ShooterSubsystem shooterSubsystem;
+
+    /*
+     * Commands ---------------------------------------------------------------
+     */
+
+    private final TeleopDriveCommand teleopDriveCmd;
+
+    private final FollowTargetCommand followTargetCmd;
+
+    private final DeployGathererCommand deployGathererCmd;
+    private final RetractGathererCommand retractGathererCmd;
+    private final BeginGatheringCommand startGathererCmd;
+
+    private final IndexerCommand runIndexerCmd;
+    private final UpperIndexerCommand runUpperIndexerCmd;
+
+    private final ShootCommand shootCmd;
+    private final ShootCommand shootBackCmd;
+    private final ShootCommand closeShootCmd;
+    private final ShootCommand closeShootBackCmd;
+
+    /*
+     * Shuffleboard -----------------------------------------------------------
+     */
+
+    /**
+     * {@link SendableChooser} for selecting the autonomous routine.
+     */
+    private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+
+    /*
+     * Supplier functions -----------------------------------------------------
+     */
+
+    /**
+     * Get the x-axis reading from the primary controller joystick.
+     * 
+     * @return the x-axis reading from the primary controller joystick
+     */
+    private double getPrimaryX() {
+        /*
+         * The x-axis on the joysticks we use is inverted.
+         */
+        return -this.primaryController.getX();
     }
 
     /**
+     * Get the y-axis reading from the primary controller joystick.
+     * 
+     * @return the y-axis reading from the primary controller joystick
+     */
+    private double getPrimaryY() {
+        return this.primaryController.getY();
+    }
+
+    /**
+     * Get the desired throttle value for use with {@link #teleopDriveCmd}.
+     * 
+     * @return the desired throttle value.
+     */
+    private double getThrottle() {
+        return this.primaryController.getRawButton(ControllerConstants.kThrottleButton) ? SLOW_THROTTLE
+                : NORMAL_THROTTLE;
+    }
+
+    /**
+     * Get whether the left trigger on the secondary controller is depressed past a
+     * certain threshold.
+     * 
+     * @return [the reading from the left trigger] > TRIGGER_THRESHOLD
+     */
+    private boolean getLeftTrigger() {
+        return this.secondaryController.getLeftTriggerAxis() > TRIGGER_THRESHOLD;
+    }
+
+    /**
+     * Get whether the right trigger on the secondary controller is depressed past a
+     * certain threshold.
+     * 
+     * @return [the reading from the right trigger] > TRIGGER_THRESHOLD
+     */
+    private boolean getRightTrigger() {
+        return this.secondaryController.getRightTriggerAxis() > TRIGGER_THRESHOLD;
+    }
+
+    /*
+     * Private helper methods -------------------------------------------------
+     */
+
+    /**
      * Use this method to define your button->command mappings. Buttons can be
-     * created by
-     * instantiating a {@link GenericHID} or one of its subclasses ({@link
-     * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing
-     * it to a {@link
-     * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     * created by instantiating a {@link GenericHID} or one of its subclasses
+     * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+     * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
      */
     private void configureButtonBindings() {
-        BooleanSupplier leftTriggerSupplier = () -> m_shooterController.getLeftTriggerAxis() > 0.3;
-        final Trigger shootTrigger = new Trigger(leftTriggerSupplier);
-        shootTrigger.whileActiveContinuous(m_shootCommand);
+        final Trigger shootTrigger = new Trigger(this::getLeftTrigger);
+        shootTrigger.whileActiveContinuous(shootCmd);
 
-        BooleanSupplier rightTriggerSupplier = () -> m_shooterController.getRightTriggerAxis() > 0.3;
-        final Trigger shootBackTrigger = new Trigger(rightTriggerSupplier);
-        shootBackTrigger.whileActiveContinuous(m_shootBackCommand);
+        final Trigger shootBackTrigger = new Trigger(this::getRightTrigger);
+        shootBackTrigger.whileActiveContinuous(shootBackCmd);
 
-        final JoystickButton closeShootBumper = new JoystickButton(m_driverStick, XboxController.Button.kLeftBumper.value);
-        closeShootBumper.whileHeld(m_closeShootCommand);
+        final JoystickButton closeShootBumper = new JoystickButton(primaryController,
+                XboxController.Button.kLeftBumper.value);
+        closeShootBumper.whileHeld(closeShootCmd);
 
-        final JoystickButton closeShootBackBumper = new JoystickButton(m_driverStick, XboxController.Button.kRightBumper.value);
-        closeShootBackBumper.whileHeld(m_closeShootBackCommand);
+        final JoystickButton closeShootBackBumper = new JoystickButton(primaryController,
+                XboxController.Button.kRightBumper.value);
+        closeShootBackBumper.whileHeld(closeShootBackCmd);
 
-        final JoystickButton gatherButton = new JoystickButton(m_driverStick, ControllerConstants.kCollectorButton);
-        final JoystickButton lowerGathererButton = new JoystickButton(m_driverStick, ControllerConstants.kLowerButton);
-        final JoystickButton raiseGathererButton = new JoystickButton(m_driverStick, ControllerConstants.kRaiseButton);
+        final JoystickButton gatherButton = new JoystickButton(primaryController, ControllerConstants.kCollectorButton);
+        final JoystickButton lowerGathererButton = new JoystickButton(primaryController,
+                ControllerConstants.kLowerButton);
+        final JoystickButton raiseGathererButton = new JoystickButton(primaryController,
+                ControllerConstants.kRaiseButton);
 
         // Temporary manual indexer controls
-        final JoystickButton runIndexerButton = new JoystickButton(m_driverStick,
+        final JoystickButton runIndexerButton = new JoystickButton(primaryController,
                 ControllerConstants.kLowerIndexerButton);
-        final JoystickButton runUpperIndexerButton = new JoystickButton(m_shooterController,
+        final JoystickButton runUpperIndexerButton = new JoystickButton(secondaryController,
                 XboxController.Button.kX.value);
-        runIndexerButton.whileHeld(m_runIndexerCommand);
-        runUpperIndexerButton.whileHeld(m_runUpperIndexerCommand);
+        runIndexerButton.whileHeld(runIndexerCmd);
+        runUpperIndexerButton.whileHeld(runUpperIndexerCmd);
 
-        final JoystickButton followTargetButton = new JoystickButton(m_driverStick,
+        final JoystickButton followTargetButton = new JoystickButton(primaryController,
                 ControllerConstants.kFollowTargetButton);
-        followTargetButton.whileHeld(m_followTargetCommand);
+        followTargetButton.whileHeld(followTargetCmd);
 
         /*
          * Hold down the gather button to deploy the gatherer and run the collector. Let
          * go to retract the gatherer and stop the collector.
          */
-        gatherButton.whileHeld(m_startGathererCommand).whenReleased(m_retractGathererCommand.withTimeout(5));
+        gatherButton.whileHeld(startGathererCmd).whenReleased(retractGathererCmd.withTimeout(5));
 
         // Temporary manual gather arm control
-        lowerGathererButton.whileHeld(m_deployGathererCommand);
-        raiseGathererButton.whenPressed(m_retractGathererCommand);
+        lowerGathererButton.whileHeld(deployGathererCmd);
+        raiseGathererButton.whenPressed(retractGathererCmd);
 
         // When limit switches are added to robot, add that to the subsystem and delete
         // withtimeout
@@ -164,13 +230,73 @@ public class RobotContainer {
         // gatherButton.whenReleased(m_retractGathererCommand.withTimeout(raiseTime));
     }
 
+    /*
+     * Constructor ------------------------------------------------------------
+     */
+
+    /**
+     * Constructs the {@link RobotContainer} object.
+     */
+    public RobotContainer() {
+        System.out.println("RobotContainer: starting initialization");
+
+        /*
+         * Initialize controllers
+         */
+
+        this.primaryController = new Joystick(PRIMARY_CONTROLLER_PORT);
+        this.secondaryController = new XboxController(SECONDARY_CONTROLLER_PORT);
+
+        /*
+         * Initialize subsystems
+         */
+
+        this.gyroSubsystem = new GyroSubsystem();
+        this.visionSubsystem = new VisionSubsystem();
+
+        this.driveTrainSubsystem = new DriveTrainSubsystem();
+
+        this.gathererSubsystem = new GathererSubsystem();
+        this.indexerSubsystem = new IndexerSubsystem();
+        this.shooterSubsystem = new ShooterSubsystem();
+
+        /*
+         * Initialize commands
+         */
+
+        this.teleopDriveCmd = new TeleopDriveCommand(this.driveTrainSubsystem,
+                this.gyroSubsystem, this::getPrimaryX, this::getPrimaryY,
+                this::getThrottle);
+
+        this.followTargetCmd = new FollowTargetCommand(this.driveTrainSubsystem,
+                visionSubsystem, this::getPrimaryX, this::getPrimaryY);
+
+        this.deployGathererCmd = new DeployGathererCommand(gathererSubsystem);
+        this.retractGathererCmd = new RetractGathererCommand(gathererSubsystem);
+        this.startGathererCmd = new BeginGatheringCommand(gathererSubsystem);
+
+        this.runIndexerCmd = new IndexerCommand(indexerSubsystem);
+        this.runUpperIndexerCmd = new UpperIndexerCommand(indexerSubsystem);
+
+        this.shootCmd = new ShootCommand(shooterSubsystem, indexerSubsystem, 0.5);
+        this.shootBackCmd = new ShootCommand(shooterSubsystem, indexerSubsystem, -0.5);
+        this.closeShootCmd = new ShootCommand(shooterSubsystem, indexerSubsystem, 0.25);
+        this.closeShootBackCmd = new ShootCommand(shooterSubsystem, indexerSubsystem,
+                -0.25);
+
+        this.driveTrainSubsystem.setDefaultCommand(this.teleopDriveCmd);
+
+        configureButtonBindings();
+
+        System.out.println("RobotContainer: initialization done");
+    }
+
     /**
      * Use this to pass the autonomous command to the main {@link Robot} class.
      *
      * @return the command to run in autonomous
      */
     public Command getAutonomousCommand() {
-        // An ExampleCommand will run in autonomous
-        return m_automonousRoutineChooser.getSelected();
+        return autoChooser.getSelected();
     }
 }
