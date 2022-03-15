a6f713c870b4185a5fdb2231f025c7a6d478efb7
Victor Chen <victorc.1@outlook.com>
2022-03-15 16:15:41 -0400

    implement PivotCommand

---
+++ src/main/java/frc/robot/commands/drive/PivotCommand.java
@@ -0,0 +1,62 @@
+package frc.robot.commands.drive;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.GyroSubsystem;
+import frc.robot.subsystems.drive.DriveTrainSubsystem;
+
+/**
+ * PID drive-straight commmand.
+ * 
+ * @author Victor Chen <victorc.1@outlook.com>
+ */
+public class PivotCommand extends CommandBase {
+
+    /*
+     * Private members --------------------------------------------------------
+     */
+
+    private final Supplier<Double> power;
+
+    private final DriveStrategy strategy;
+
+    /*
+     * Constructor ------------------------------------------------------------
+     */
+
+    /**
+     * Constructs a new {@link PivotCommand}.
+     * 
+     * @param drive the {@link DriveTrainSubsystem} to control
+     * @param power the percentage power to pivot at; positive values correspond to
+     *              clockwise rotation
+     * @requires -1 <= [the return value of power] <= 1
+     */
+    public PivotCommand(DriveTrainSubsystem drive, Supplier<Double> power) {
+        this.power = power;
+        this.strategy = new PivotTurnStrategy(drive);
+
+        addRequirements(drive);
+    }
+
+    /*
+     * Command methods --------------------------------------------------------
+     */
+
+    @Override
+    public void initialize() {
+        this.strategy.reset();
+    }
+
+    @Override
+    public void execute() {
+        this.strategy.execute(this.power.get(), 0);
+    }
+
+    @Override
+    public void end(boolean isFinished) {
+
+    }
+
+}
