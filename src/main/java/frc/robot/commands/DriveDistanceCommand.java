b7b12c3b50750ca8f0780a1e219f59001b2801bd
Victor Chen <victorc.1@outlook.com>
2022-03-15 16:48:30 -0400

    Update DriveDistanceCommand

--- src/main/java/frc/robot/commands/DriveDistanceCommand.java
+++ src/main/java/frc/robot/commands/DriveDistanceCommand.java
@@ -1,52 +1,110 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.drive.DriveTrainSubsystem;
+import frc.robot.commands.drive.DriveStraightStrategy;
+import frc.robot.commands.drive.DriveStrategy;
 import frc.robot.subsystems.GyroSubsystem;
 
-public class DriveDistanceCommand extends CommandBase{
-    private final DriveTrainSubsystem m_driveSubsystem;
-    private final GyroSubsystem m_gyro;
-    
-    private double startTime;
-    private double seconds;
-    private double speed;
-    /**
-     * Creates a new ExampleCommand.
-     *
-     * @param subsystem The subsystem used by this command.
+/**
+ * This command is used to drive forward a set distance.
+ * 
+ */
+public class DriveDistanceCommand extends CommandBase {
+
+    /*
+     * Private constants ------------------------------------------------------
      */
-    public DriveDistanceCommand(DriveTrainSubsystem subsystem, GyroSubsystem gyro, double distance) {
-      this.m_driveSubsystem = subsystem;
-      this.m_gyro = gyro;
 
-      // TODO: Replace 5.0 with actual speed at .3 power using regression
-      this.speed = .3;
-      if(distance < 0){
-        speed = -speed;
-      }
-      this.seconds = distance / 5.0;
+    /**
+     * The power to drive the robot at.
+     */
+    private final double POWER = 0.3;
 
-      // Use addRequirements() here to declare subsystem dependencies.
-      addRequirements(m_driveSubsystem, m_gyro);
+    /**
+     * The speed of the robot when driven at {@link #POWER}.
+     */
+    private final double SPEED = 5.0; // TODO: Measure what this should be
+
+    /*
+     * Private members --------------------------------------------------------
+     */
+
+    private final DriveTrainSubsystem drive;
+    private final GyroSubsystem gyro;
+
+    private final Timer timer;
+
+    private final boolean forwards;
+    private final double time;
+
+    private final DriveStrategy strategy;
+
+    /*
+     * Private helper methods -------------------------------------------------
+     */
+
+    /**
+     * Calculates the time required to move a certain distance at a certain speed.
+     * 
+     * @param distance the distance to traverse
+     * @param speed    the speed to move at
+     * @return the amount of time it takes to move {@code distance} at {@code speed}
+     * @requires distance >= 0 and speed > 0
+     */
+    private static double calculateTime(double distance, double speed) {
+        return distance / speed;
     }
-  
-    // Called when the command is initially scheduled.
+
+    /*
+     * Constructor ------------------------------------------------------------
+     */
+
+    /**
+     * Constructs a new {@link DriveDistanceCommand}.
+     * 
+     * @param subsystem the {@link DriveTrainSubsystem} to control
+     * @param gyro      the {@link GyroSubsystem} to read from
+     * @param distance  the distance (in what units???) to move; positive is
+     *                  forwards
+     */
+    public DriveDistanceCommand(DriveTrainSubsystem drive, GyroSubsystem gyro, double distance) {
+        this.drive = drive;
+        this.gyro = gyro;
+
+        this.timer = new Timer();
+
+        this.forwards = distance >= 0;
+        this.time = calculateTime(distance, SPEED);
+
+        this.strategy = new DriveStraightStrategy(drive, gyro);
+
+        addRequirements(this.drive, this.gyro);
+    }
+
     @Override
     public void initialize() {
-        this.startTime = System.currentTimeMillis();
-    }
-  
-    // Called every time the scheduler runs while the command is scheduled.
-    @Override
-    public void execute() {
-        DriveStrategyCommand.driveStraight(m_driveSubsystem, m_gyro, () -> speed).withTimeout(seconds);
+        this.strategy.reset();
+        this.timer.reset();
     }
 
-  
-    // Returns true when the command should end.
+    @Override
+    public void execute() {
+        if (this.forwards) {
+            this.strategy.execute(0, POWER);
+        } else {
+            this.strategy.execute(0, -POWER);
+        }
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        this.drive.stop();
+    }
+
     @Override
     public boolean isFinished() {
-        return (System.currentTimeMillis() - startTime) > seconds;
+        return this.timer.get() > this.time;
     }
 }
