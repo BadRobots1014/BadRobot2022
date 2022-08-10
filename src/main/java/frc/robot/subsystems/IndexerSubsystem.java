fac30f3938565cef279d19b777b063761a0476d1
Fyrefyte <leowilliamcanales@gmail.com>
2022-08-10 18:48:54 -0400

    Added functionality and motor control

--- src/main/java/frc/robot/subsystems/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -5,6 +5,7 @@ import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
 import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.IndexerConstants;
 
@@ -22,6 +23,7 @@ public class IndexerSubsystem extends SubsystemBase {
 
     private String m_state;
     public boolean m_shotRequested;
+    private Timer m_timer;
 
     private final ShuffleboardTab tab;
 
@@ -29,6 +31,7 @@ public class IndexerSubsystem extends SubsystemBase {
         m_state = "empty";
         checkState();
         m_shotRequested = false;
+        m_timer = new Timer();
 
         this.tab = Shuffleboard.getTab("Indexer");
         this.tab.addBoolean("Lower Sensor", m_lowerSensorSupplier);
@@ -38,6 +41,7 @@ public class IndexerSubsystem extends SubsystemBase {
 
     public void periodic() {
         checkState();
+        actOnState();
     }
 
     public void runLowerMotor(double power) {
@@ -60,6 +64,13 @@ public class IndexerSubsystem extends SubsystemBase {
         System.out.println("Stopped upper motor");
     }
 
+    public void stopAll() {
+        m_upperMotor.set(ControlMode.PercentOutput, 0);
+        System.out.println("Stopped upper motor");
+        m_lowerMotor.set(ControlMode.PercentOutput, 0);
+        System.out.println("Stopped lower motor");
+    }
+
     public boolean lowerSensorOn() {
         return m_lowerSensor.get();
     }
@@ -68,33 +79,78 @@ public class IndexerSubsystem extends SubsystemBase {
         return m_upperSensor.get();
     }
 
-    public void checkState() {
-        if (m_state.equals("empty")) {
-            if (lowerSensorOn()) {
-                m_state = "emptyIntaking";
-            }
+    public void checkState() { //Moves through the natural state flow
+        switch(m_state) {
+            case "empty":
+                if (lowerSensorOn()) {
+                    m_state = "emptyIntaking";
+                }
+            break;
+            case "emptyIntaking":
+                if (upperSensorOn()) {
+                    m_state = "topLoaded";
+                }
+                //TODO Stop if the ball rolls out? (Not for now)
+            break;
+            case "toploaded":
+                if (!upperSensorOn()) {
+                    m_state = "empty";
+                }
+                else if (lowerSensorOn()) {
+                    m_state = "topLoadedIntaking";
+                    m_timer.stop();
+                    m_timer.reset();
+                    m_timer.start();
+                }
+            break;
+            case "topLoadedIntaking":
+                if (!lowerSensorOn()) {
+                    m_timer.stop();
+                    m_state = "topLoaded";
+                }
+                else if (m_timer.hasElapsed(2.0)) {
+                    m_timer.stop();
+                    m_state = "fullLoaded";
+                }
+            break;
+            case "fullLoaded":
+                if (!upperSensorOn()) {
+                    m_state = "emptyIntaking";
+                }
+                else if (!lowerSensorOn()) {
+                    m_state = "topLoaded";
+                }
+            break;
         }
-        else if (m_state.equals("emptyIntaking")) {
-            if (upperSensorOn()) {
-                m_state = "topLoaded";
-            }
-            //TODO Stop if the ball rolls out?
+    }
+
+    public void jumpState() { //Gets the state by raw sensor data, not state flow
+        if (upperSensorOn()) {
+            if (lowerSensorOn()) m_state = "fullLoaded";
+            else m_state = "topLoaded";
         }
-        else if (m_state.equals("topLoaded")) {
-            if (m_shotRequested) {
-                m_state = "topLoadedShotReq";
-            }
-            else if (lowerSensorOn()) {
-                m_state = "topLoadedIntaking";
-            }
+        else {
+            if (lowerSensorOn()) m_state = "emptyIntaking";
+            else m_state = "empty";
         }
-        else if (m_state.equals("topLoadedIntaking")) {
-            if (!lowerSensorOn()) {
-                m_state = "topLoaded";
-            }
-            //Time passes: m_state = "fullLoaded";
-        }
-        else if (m_state.equals("fullLoaded")) {
+        //Does not include "topLoadedIntaking" state.
+    }
+
+    public void overideState(String newState) { //Sets the state
+        m_state = newState;
+    }
+
+    public void actOnState() {
+        switch(m_state) {
+            case "empty":
+            case "topLoaded":
+            case "fullLoaded":
+                stopLowerMotor();
+            break;
+            case "emptyIntaking":
+            case "topLoadedIntaking":
+                runLowerMotor(1);
+            break;
         }
     }
 }
